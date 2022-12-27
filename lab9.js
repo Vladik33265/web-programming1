c0f5403613598c9187ff0c5a1f707526e11d900c
Владислав Астанин <vlad-astanin2002@mail.ru>
2022-12-28 02:59:16 +0700

    Переключения состояний

--- lab9.js
+++ lab9.js
@@ -10,3 +10,20 @@ function clickName(button){
     else
         button.innerHTML = 'Владислав';
 }
+function clickMe2(button){
+    if( button.innerHTML == 'Нажми меня'){
+        button.innerHTML = 'Ещё раз нажми';
+        button.style.color = 'DarkSlateBlue';
+    }
+    else if( button.innerHTML == 'Ещё раз нажми'){
+        button.innerHTML = 'Ещё';
+        button.style.color = 'Violet';}
+    else if( button.innerHTML == 'Ещё'){
+        button.innerHTML = 'Начать сначала';
+        button.style.color = 'BlueViolet';}
+    else if( button.innerHTML == 'Начать сначала'){
+        button.innerHTML = 'Нажми меня';
+        button.style.color = 'Purple';}
+    else
+        button.innerHTML = 'Нажми меня';
+}
